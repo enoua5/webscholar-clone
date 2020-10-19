248fa0b3ee7efb8b44f8a101895940d8867cbace
ethanchaiko <ethanchaiko@gmail.com>
2020-10-19 16:44:51 -0600

    initial docker,yml, and pom files added.

--- account-service/src/main/java/edu/weber/App.java
+++ account-service/src/main/java/edu/weber/App.java
@@ -1,10 +1,16 @@
 package edu.weber;
 
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * Hello world!
  *
  */
-public class App 
+
+@SpringCloudApplication
+@EnableDiscoveryClient
+public class App
 {
     public static void main( String[] args )
     {

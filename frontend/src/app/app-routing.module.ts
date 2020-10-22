f29c34d0da1a58d98dcfda4668003cbf7fe4b2ae
annaek <annaek@mail.weber.edu>
2020-10-21 21:19:14 -0600

    Add login-form, remove test components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RegisterFormComponent} from "./register-form/register-form.component";
+import {LoginFormComponent} from "./login-form/login-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginFormComponent },
+  { path: 'register', component: RegisterFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

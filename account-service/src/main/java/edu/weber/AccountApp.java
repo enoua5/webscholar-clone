d5c883322a36f3551b5245a7098a37577000e19a
ethanchaiko <ethanchaiko@gmail.com>
2020-10-28 20:18:55 -0600

    config-service, registry, and account-service are now running without errors.
    registry and account-service now successfully retrieve their profiles from config-service.

--- account-service/src/main/java/edu/weber/AccountApp.java
+++ account-service/src/main/java/edu/weber/AccountApp.java
@@ -1,13 +1,15 @@
 package edu.weber;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringCloudApplication
-@EnableCircuitBreaker
+@SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
 public class AccountApp
 {
     public static void main( String[] args )

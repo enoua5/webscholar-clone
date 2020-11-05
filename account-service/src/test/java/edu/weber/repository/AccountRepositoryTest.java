b20adc9be6634997e04bb69c78fe0ec62bd4608b
ethanchaiko <ethanchaiko@gmail.com>
2020-11-04 22:03:13 -0700

    tried to connect to the db with jdbc and jpa.

---
+++ account-service/src/test/java/edu/weber/repository/AccountRepositoryTest.java
@@ -0,0 +1,8 @@
+package edu.weber.repository;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+public class AccountRepositoryTest {
+}

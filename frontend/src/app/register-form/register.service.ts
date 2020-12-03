d5061f7683f20668cd7be65253a738d8fe131284
annaek <annaek@mail.weber.edu>
2020-12-02 20:01:57 -0700

    remove headers from frontend request

--- frontend/src/app/register-form/register.service.ts
+++ frontend/src/app/register-form/register.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-const INSERT_URL = 'http://localhost:8000/register.php';
+const INSERT_URL = 'http://localhost:6001/register.php';
 
 @Injectable({
   providedIn: 'root',
@@ -13,12 +13,7 @@ export class RegisterService{
   }
 
   public insert(data): Observable<any> {
-    let headers = new HttpHeaders({
-      'Access-Control-Allow-Origin' : '*',
-      'Access-Control-Allow-Methods' : 'GET,POST,PUT,PATCH',
-      'Access-Control-Allow-Headers' : 'content-type'
-    })
-    return this.http.post(INSERT_URL, data, {headers: headers});
+    return this.http.post(INSERT_URL, data);
   }
 
 }

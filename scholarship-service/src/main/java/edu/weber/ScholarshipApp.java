9d3a64129bc05e2dbe79f3e7de51b7888dc48362
LAPTOP-V6917H3A\Gobbo <Peterburnett1@mail.weber.edu>
2021-04-01 09:35:51 -0600

    Scholarship Controller work in progress

--- scholarship-service/src/main/java/edu/weber/ScholarshipApp.java
+++ scholarship-service/src/main/java/edu/weber/ScholarshipApp.java
@@ -1,13 +1,27 @@
 package edu.weber;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+//@EnableJpaRepositories(basePackages = {"edu.weber.service"})
+@ComponentScan(basePackages = {"edu.weber.service","edu.weber"})
+@EnableJpaRepositories("edu.weber.repository")
+@EntityScan(basePackages = "edu.weber.model")
+
 public class ScholarshipApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(ScholarshipApp.class, args);
     }
 }

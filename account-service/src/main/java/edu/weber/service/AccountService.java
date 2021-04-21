2d8cb4023af8f2c3506dccdb1e8d47356ea0a31e
D Gray <dariusgray@mail.weber.edu>
2021-04-20 21:22:39 -0600

    account-service email API created

--- account-service/src/main/java/edu/weber/service/AccountService.java
+++ account-service/src/main/java/edu/weber/service/AccountService.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * This class extends the functionality of 'AccountRepository'. Instead of using the default
@@ -14,6 +17,21 @@ import org.springframework.util.Assert;
  */
 @Service
 public class AccountService {
+
+    /**
+     * This variable pulls in the smtp server username from
+     * the account-service bootstrap.yml.
+     * If the username in bootstrap.yml changes, the new
+     * username will be used here automatically.
+     */
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+    /**
+     * This object is used to send emails in the sendInvite() method
+     */
+    @Autowired
+    private JavaMailSender emailSender;
 
     /**
      * This is the logger which uses the slf4j logging facade API.
@@ -62,6 +80,53 @@ public class AccountService {
 
         //Save the updated account
         accountRepository.save(account);
+
+        return true;
+    }
+
+
+    /**
+     * This method sends an email message that includes the sender's name
+     *
+     * @param accountKey        The id for the sender's account.
+     * @param recipientEmail    The destination email address
+     * @return Returns a success or fail flag depending on if the sender's account can be found.
+     *
+     * Note:  The sender's email address is defined in the variable senderEmail
+     * at the top of this class.
+     */
+    public boolean sendInvite(int accountKey, String recipientEmail) {
+
+        //Get the sender's account
+        Account account = accountRepository.findAccountByAccountKey(accountKey);
+
+        //Verify the sender's account exists
+        if(account == null){
+
+            // Log Error
+            log.error("ERROR: Account Number " + accountKey + " not found -- SOURCE: sendInvite()");
+
+            return false;
+        }
+
+        // Get email sender's name to use in message body
+        String senderName = account.getFirstName() + " " + account.getLastName();
+
+        String messageBody = senderName + " has sent you an invite!\n";
+        String messageSubject = "Webscholar Mail Test";
+        
+        // Create mail message object
+        SimpleMailMessage message = new SimpleMailMessage();
+
+
+        // Set message attributes
+        message.setFrom(senderEmail);
+        message.setTo(recipientEmail);
+        message.setSubject(messageSubject);
+        message.setText(messageBody);
+        
+        //Send the message
+        emailSender.send(message);
 
         return true;
     }

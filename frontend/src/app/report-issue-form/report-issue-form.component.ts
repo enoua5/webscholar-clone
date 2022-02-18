648e124103210be0d74527ec83521ac569f366a0
Anthony Bahl <anthonybahl@mail.weber.edu>
2022-02-17 22:16:52 -0700

    Added Report an Issue Form.

--- frontend/src/app/report-issue-form/report-issue-form.component.ts
+++ frontend/src/app/report-issue-form/report-issue-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {ActivatedRoute, Router} from "@angular/router";
-import {RegisterService} from "../register-form/register.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ReportIssueFormService} from "./report-issue-form.service";
 
 @Component({
   selector: 'app-report-issue-form',
@@ -9,91 +8,132 @@ import {RegisterService} from "../register-form/register.service";
   styleUrls: ['./report-issue-form.component.less']
 })
 export class ReportIssueFormComponent implements OnInit {
-  form: FormGroup;
-  error: string = null;
-  errors: Map<string, string> = new Map();
+  reportIssueForm: FormGroup;
+  severityList: string[] = [
+    'Critical',
+    'Major',
+    'Moderate',
+    'Minor',
+    'Cosmetic'
+  ];
+  priorityList: string[] = [
+    'Low',
+    'Medium',
+    'High'
+  ]
+  errorMessage: string = "";
+  validMessage: string = "";
 
-  constructor(
-    private fb: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private service: RegisterService) {
-    this.form = this.fb.group({
-      issueName: [''],
-      email: [''],
-      issueType: [''],
-      issueDescription: ['']
-    });
-  }
+
+  constructor(private reportIssueFormService: ReportIssueFormService) { }
 
   ngOnInit(): void {
+    this.reportIssueForm = new FormGroup({
+      reporter: new FormControl('', Validators.required),
+      summary: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required)
+    })
   }
 
-  private checkErrors(): void {
-    this.errors.clear();
-
-    //Issue Name verification
-    if (this.form.get('issueName').value.length == 0) {
-      this.errors.set('issueName', 'Invalid Issue Name');
-    }
-
-    //Email verification
-    const validEmail = RegExp('^[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$');
-    if (!validEmail.test(this.form.get('email').value) || this.form.get('email').value.length == 0) {
-      this.errors.set('email', 'Invalid email format');
-    }
-
-    //Issue Type Verification
-    if(this.form.get('issueType').value == ""){
-      this.errors.set ('issueType', 'You Must Select an Issue Type')
-    }
-
-    //issue Description Verification
-    if (this.form.get('issueDescription').value.length == 0) {
-      this.errors.set('issueDescription', 'You Must Describe the Issue');
-    }
-
-  }
-
-  onSubmit(): void {
-
-    console.log(this.form.value);
-    this.checkErrors();
-
-    const issueName = this.form.get('issueName').value;
-    const email = this.form.get('email').value;
-    const issueType = this.form.get('issueType').value;
-    const issueDescription = this.form.get('issueDescription').value;
-
-
-    if(this.errors.size == 0){
-      // this.service.insert(this.form.value)
-      //   .subscribe((data) => this.processResponse(data));
-      const jsonObj = JSON.stringify({
-        issueName: issueName,
-        issueType: issueType,
-        email: email,
-        issueDescription: issueDescription
-
-      });
-
-      console.log(jsonObj);
-      //this.service.testAccountCreation().subscribe((data) => this.processResponse(data));
-
-    }
-
-  }
-
-  private processResponse(data) {
-    console.log(data);
-    if (data.success == true) {
-      this.router.navigate(['/dashboard']);
-    }
-    else {
-      console.warn('Else Statement executed');
-      this.error = data.error;
-      this.errors = new Map(Object.entries(data.errors));
+  SubmitIssue() {
+    if (this.reportIssueForm.valid) {
+      this.errorMessage = "";
+      this.validMessage = "";
+      this.reportIssueFormService.createIssue(this.reportIssueForm.value).subscribe(
+        data => {
+          this.reportIssueForm.reset();
+          this.validMessage = "Your issue has been reported. Thank you!";
+          return true;
+        },
+        error => {
+          this.errorMessage = error;
+        }
+      )
+    } else {
+      this.validMessage = "";
+        this.errorMessage = "Please fill out the form before submitting!";
     }
   }
 
+  // form: FormGroup;
+  // errors: Map<string, string> = new Map();
+  // constructor(
+  //   private fb: FormBuilder,
+  //   private route: ActivatedRoute,
+  //   private router: Router,
+  //   private service: RegisterService) {
+  //   this.form = this.fb.group({
+  //     issueName: [''],
+  //     email: [''],
+  //     issueType: [''],
+  //     issueDescription: ['']
+  //   });
+  // }
+  //
+  // private checkErrors(): void {
+  //   this.errors.clear();
+  //
+  //   //Issue Name verification
+  //   if (this.form.get('issueName').value.length == 0) {
+  //     this.errors.set('issueName', 'Invalid Issue Name');
+  //   }
+  //
+  //   //Email verification
+  //   const validEmail = RegExp('^[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$');
+  //   if (!validEmail.test(this.form.get('email').value) || this.form.get('email').value.length == 0) {
+  //     this.errors.set('email', 'Invalid email format');
+  //   }
+  //
+  //   //Issue Type Verification
+  //   if(this.form.get('issueType').value == ""){
+  //     this.errors.set ('issueType', 'You Must Select an Issue Type')
+  //   }
+  //
+  //   //issue Description Verification
+  //   if (this.form.get('issueDescription').value.length == 0) {
+  //     this.errors.set('issueDescription', 'You Must Describe the Issue');
+  //   }
+  //
+  // }
+  //
+  // onSubmit(): void {
+  //
+  //   console.log(this.form.value);
+  //   this.checkErrors();
+  //
+  //   const issueName = this.form.get('issueName').value;
+  //   const email = this.form.get('email').value;
+  //   const issueType = this.form.get('issueType').value;
+  //   const issueDescription = this.form.get('issueDescription').value;
+  //
+  //
+  //   if(this.errors.size == 0){
+  //     // this.service.insert(this.form.value)
+  //     //   .subscribe((data) => this.processResponse(data));
+  //     const jsonObj = JSON.stringify({
+  //       issueName: issueName,
+  //       issueType: issueType,
+  //       email: email,
+  //       issueDescription: issueDescription
+  //
+  //     });
+  //
+  //     console.log(jsonObj);
+  //     //this.service.testAccountCreation().subscribe((data) => this.processResponse(data));
+  //
+  //   }
+  //
+  // }
+  //
+  // private processResponse(data) {
+  //   console.log(data);
+  //   if (data.success == true) {
+  //     this.router.navigate(['/dashboard']);
+  //   }
+  //   else {
+  //     console.warn('Else Statement executed');
+  //     this.error = data.error;
+  //     this.errors = new Map(Object.entries(data.errors));
+  //   }
+  // }
 }
---
+++ frontend/src/app/report-issue-form/report-issue-form.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportIssueFormService {
+
+  constructor(private http: HttpClient) { }
+
+  createIssue(issue) {
+    let body = JSON.stringify(issue);
+    // ToDo: Replace with API Call once backend team sets up API call to create an issue
+    return this.http.post('{{placeholder}}', body, httpOptions);
+  }
+}

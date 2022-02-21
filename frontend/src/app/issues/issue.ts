395369d59a97d133e83db287906839a249379e57
Anthony Bahl <anthonybahl@mail.weber.edu>
2022-02-21 14:28:43 -0700

    Added Issue interface

---
+++ frontend/src/app/issues/issue.ts
@@ -0,0 +1,10 @@
+export interface Iissue {
+  issueID: number;
+  issueStatus: string;
+  issueSummary: string;
+  issueDescription: string;
+  issueSeverity: string;
+  issuePriority: string;
+  issueReporter: number;
+  issueWorker: number;
+}

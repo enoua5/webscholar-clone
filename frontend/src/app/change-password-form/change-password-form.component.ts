d593572d45377542b7ae27fbd09a99ea05e066ef
Duy Nguyen <duynguyen1@mail.weber.edu>
2022-03-17 18:37:14 -0600

    Update change password form to use Bootstrap form

--- frontend/src/app/change-password-form/change-password-form.component.ts
+++ frontend/src/app/change-password-form/change-password-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {passwordMatchValidator} from "./validators";
 
 @Component({
   selector: 'app-change-password-form',
@@ -6,10 +8,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password-form.component.less']
 })
 export class ChangePasswordFormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.form = this.formBuilder.group({
+        current_password: ['', {
+          validators: [
+            Validators.required,
+            Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%?&])[A-Za-z\\d$@$!%?&]{8,}$')
+          ]
+        }],
+        passwordFields: this.formBuilder.group({
+          new_password: ['', {
+            validators: [
+              Validators.required,
+              Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%?&])[A-Za-z\\d$@$!%?&]{8,}$')
+            ]
+          }],
+          confirm_password: ['', {
+            validators: [
+              Validators.required
+            ]
+          }]
+        }, {
+          validators: [
+            passwordMatchValidator()
+          ]
+        })
+      }, {
+        updateOn: 'blur'
+      });
+  }
+
+  get current_password() {
+    return this.form.get('current_password');
+  }
+
+  get passwordFields() {
+    return this.form.get('passwordFields');
+  }
+
+  get new_password() {
+    return this.passwordFields.get('new_password');
+  }
+
+  get confirm_password() {
+    return this.passwordFields.get('confirm_password');
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    // Do something here
+  }
 }

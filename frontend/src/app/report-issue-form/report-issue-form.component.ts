bebfe091b7ee73b932f177e45cb07adaa5b3f8d0
Anthony Bahl <anthonybahl@mail.weber.edu>
2022-03-23 22:26:20 -0600

    Modified the report-issues-form page.

--- frontend/src/app/report-issue-form/report-issue-form.component.ts
+++ frontend/src/app/report-issue-form/report-issue-form.component.ts
@@ -1,8 +1,9 @@
 // ToDO: Limit page to only be viewable if the user is logged in.
 // ToDO: Redirect back to list of issues once that page is set up.
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ReportIssueFormService} from "./report-issue-form.service";
+import {emailTakenValidator, passwordMatchValidator} from "../register-form/validators";
 
 @Component({
   selector: 'app-report-issue-form',
@@ -10,6 +11,7 @@ import {ReportIssueFormService} from "./report-issue-form.service";
   styleUrls: ['./report-issue-form.component.less']
 })
 export class ReportIssueFormComponent implements OnInit {
+  // Reactive form
   reportIssueForm: FormGroup;
   severityList: string[] = [
     'Critical',
@@ -25,19 +27,61 @@ export class ReportIssueFormComponent implements OnInit {
   ]
   errorMessage: string = "";
   validMessage: string = "";
+  SignedIn: boolean = true;
 
 
-  constructor(private reportIssueFormService: ReportIssueFormService) {
+  constructor(
+    private reportIssueFormService: ReportIssueFormService,
+    private formBuilder: FormBuilder) {
+    this.reportIssueForm = this.formBuilder.group({
+      username: ['', {validators: [Validators.required]}],
+      summary: ['', {
+        validators: [
+          Validators.required,
+          Validators.pattern('^.{15,}$')
+        ]}],
+      description: ['', {
+        validators: [
+          Validators.required,
+          Validators.pattern('^.{50,}$')
+        ]}],
+      recreate: ['', {
+        validators: [
+          Validators.required,
+          Validators.pattern('^.{15,}$')
+        ]}],
+      severity: ['', {validators: [Validators.required]}],
+      priority: ['', {validators: [Validators.required]}],
+    }, {
+      updateOn: 'blur'
+    });
+  }
+
+  get username() {
+    return this.reportIssueForm.get('username');
+  }
+
+  get summary() {
+    return this.reportIssueForm.get('summary');
+  }
+
+  get description() {
+    return this.reportIssueForm.get('description');
+  }
+
+  get recreate() {
+    return this.reportIssueForm.get('recreate');
+  }
+
+  get severity() {
+    return this.reportIssueForm.get('severity');
+  }
+
+  get priority() {
+    return this.reportIssueForm.get('priority');
   }
 
   ngOnInit(): void {
-    this.reportIssueForm = new FormGroup({
-      reporter: new FormControl('', Validators.required),
-      summary: new FormControl('', Validators.required),
-      description: new FormControl('', Validators.required),
-      severity: new FormControl('', Validators.required),
-      priority: new FormControl('', Validators.required)
-    })
   }
 
   SubmitIssue() {

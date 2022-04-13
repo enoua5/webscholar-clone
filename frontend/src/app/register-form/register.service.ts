5dcb8a3a2048e1bc550e60485040f79e100466aa
Anthony Bahl <anthonybahl@mail.weber.edu>
2022-04-13 16:24:32 -0600

    fix to counter merge conflicts

--- frontend/src/app/register-form/register.service.ts
+++ frontend/src/app/register-form/register.service.ts
@@ -2,12 +2,12 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders, HttpRequest, HttpHeaderResponse} from "@angular/common/http";
 import {Observable} from "rxjs";
 
+const LOGIN_URL = 'http://localhost:8000/login.php';
 // const INSERT_URL = 'http://localhost:6001/accounts';
 const headerDict = {
   'Content-Type': 'application/json',
   'Accept': 'application/json',
   'Access-Control-Allow-Headers': 'Content-Type',
-
 }
 
 @Injectable({
@@ -21,35 +21,34 @@ export class RegisterService{
 
   // This creates a test account
   //public testAccountCreation(){
-    //let header = new HttpHeaders({  'Content-Type': 'application/json',
-      //'Accept': 'application/json',
-      //'Access-Control-Allow-Headers': 'Content-Type',
-    //})
-    //Testing using make_test_account post method
-    //return this.http.post('http://localhost:6001/account/make_test_account',{headers: header}, { observe: 'response', responseType: 'text'}).subscribe();
+  //let header = new HttpHeaders({  'Content-Type': 'application/json',
+  //'Accept': 'application/json',
+  //'Access-Control-Allow-Headers': 'Content-Type',
+  //})
+  //Testing using make_test_account post method
+  //return this.http.post('http://localhost:6001/account/make_test_account',{headers: header}, { observe: 'response', responseType: 'text'}).subscribe();
   //}
 
   // This method is for account creation
   public createAccount(data){
-     let header = new HttpHeaders({  'Content-Type': 'application/json',
-       'Accept': 'application/json',
-       'Access-Control-Allow-Headers': 'Content-Type',
-     });
+    let header = new HttpHeaders({  'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    });
 
-     //checking if data is not null
-     console.log(data);
+    //checking if data is not null
+    console.log(data);
 
-     return this.http.post<any>('http://localhost:6001/account/create', data, { headers: header, observe: 'response', responseType: 'json'}).pipe();
+    return this.http.post<any>('http://localhost:6001/account/create', data, { headers: header, observe: 'response', responseType: 'json'}).pipe();
   }
 
-
-  public emailTaken(email: string) {
-    return this.http.get('http://localhost:6001/account/emailTaken', {params: {email: email}}).pipe();
+  public emailExists(email: string) {
+    return this.http.get('http://localhost:6001/account/emailExists', {params: {email: email}}).pipe();
   }
 
   //Not sure what this method was doing with the old INSERT_URL
   // public insert(data): Observable<any> {
   //   return this.http.post(INSERT_URL, data);
- //}
+  //}
 
 }

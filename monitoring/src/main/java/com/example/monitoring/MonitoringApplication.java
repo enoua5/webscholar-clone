edae1b276de887785423ecea850998a627e43547
Ryan Dickson <ryandickson@mail.weber.edu>
2022-12-07 21:10:15 -0700

    monitoring

--- monitoring/src/main/java/com/example/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/example/monitoring/MonitoringApplication.java
@@ -2,12 +2,14 @@ package com.example.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
 
 @SpringBootApplication
+@EnableHystrixDashboard
 public class MonitoringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MonitoringApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringApplication.class, args);
+    }
 }

ff7c28e329ec76e47861191e2649644c5bc0d18e
Yong Zhang <yongzhang@weber.edu>
2023-01-18 16:40:58 -0700

    merge conflicts.

--- account-service/src/main/java/edu/weber/AccountApp.java
+++ account-service/src/main/java/edu/weber/AccountApp.java
@@ -1,5 +1,7 @@
 package edu.weber;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -16,14 +18,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableFeignClients
 @EnableCircuitBreaker
 //@EnableJpaRepositories(basePackages = {"edu.weber.service"})
-@ComponentScan(basePackages = {"edu.weber.service","edu.weber"})
+//@ComponentScan(basePackages = {"edu.weber.service","edu.weber"})
 @EnableJpaRepositories("edu.weber.repository")
 @EntityScan(basePackages = "edu.weber.model")
 
 public class AccountApp
 {
+    private static final Logger logM = LoggerFactory.getLogger(AccountApp.class);
     public static void main( String[] args )
     {
+        logM.debug("AccountApp.main() Started Debug");
         SpringApplication.run(AccountApp.class, args);
     }
 

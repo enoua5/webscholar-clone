c463d0317bcba5fb8553adb5558c1e189bf00482
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-02-03 16:03:24 -0700

    packaging and spring.run
    set packaging type to war
    running the auth application rather than its config

--- auth-service/src/main/java/edu/weber/auth/config/AuthApplication.java
+++ auth-service/src/main/java/edu/weber/auth/config/AuthApplication.java
@@ -3,6 +3,7 @@ package edu.weber.auth.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import edu.weber.auth.config.*;
@@ -14,7 +15,7 @@ public class AuthApplication {
 
 	public static void main(String[] args) {
 		System.setProperty("com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize", "true");
-		SpringApplication.run(AuthConfig.class, args);
+		SpringApplication.run(AuthApplication.class, args);
 	}
 
 }

2f4d114ddb3d1c02d5446a36635f5663f07f3619
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-02-03 21:19:24 -0700

    database connection
    now seems to be able to connect to mysql database i think

--- auth-service/src/main/java/edu/weber/auth/config/AuthApplication.java
+++ auth-service/src/main/java/edu/weber/auth/config/AuthApplication.java
@@ -2,6 +2,7 @@ package edu.weber.auth.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -10,6 +11,8 @@ import edu.weber.auth.config.*;
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
+@EnableJpaRepositories("edu.weber.auth.repository")
+@EntityScan(basePackages = "edu.weber.auth.model")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthApplication {
 

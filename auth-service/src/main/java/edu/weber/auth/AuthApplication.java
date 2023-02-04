2d5e72609c1df845072221f18bc0d9827cc9f3e1
Dante Atkinson <97570073+Dlatki6@users.noreply.github.com>
2023-02-03 23:01:04 -0700

    Running auth application
    Application can now successfully start and run

--- auth-service/src/main/java/edu/weber/auth/AuthApplication.java
+++ auth-service/src/main/java/edu/weber/auth/AuthApplication.java
@@ -1,16 +1,18 @@
-package edu.weber.auth.config;
+package edu.weber.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import edu.weber.auth.config.*;
+
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"edu.weber.auth.service","edu.weber.auth"})
 @EnableJpaRepositories("edu.weber.auth.repository")
 @EntityScan(basePackages = "edu.weber.auth.model")
 @EnableGlobalMethodSecurity(prePostEnabled = true)

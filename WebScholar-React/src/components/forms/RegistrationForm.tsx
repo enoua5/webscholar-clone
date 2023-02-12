04cc627a7befb4aaa610452b5a0de41928d9d6ec
BrandonShapiro <brandonshapiro9@gmail.com>
2023-02-11 21:34:16 -0700

    added simple error handling on registration page, updated logo

--- WebScholar-React/src/components/forms/RegistrationForm.tsx
+++ WebScholar-React/src/components/forms/RegistrationForm.tsx
@@ -20,42 +20,94 @@ export default function RegistrationForm() {
     role: "student"
   })
 
+  const [error, setError] = useState(false);
+  const [errorText, setErrorText] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  function checkPassword(password: string){
+    var regularExpression = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return regularExpression.test(password);
+  }
+
+  //this kind of validation should probably happen on the api
+  const register = (user: User) => {
+    setError(false)
+    if(user.firstName === ""){
+      setError(true);
+      setErrorText("Please provide your first name");
+      return;
+    } else if (user.lastName === ""){
+      setError(true);
+      setErrorText("Please provide your last name");
+      return;
+    } else if(user.email === "" || !['.com', '.org', '.edu', '.net'].some(domain => user.email.includes(domain))){
+      setError(true);
+      setErrorText("Please provide a valid email address");
+      return;
+    } else if(!checkPassword(user.password)){
+      setError(true);
+      setErrorText("Password must be at least eight characters, contain one uppercase letter, one lowercase letter, one number and one special character");
+      return;
+    } else if(confirmPassword !== user.password) {
+      setError(true);
+      setErrorText("Passwords must match")
+    } else {
+      setError(false)
+      // call the API to create a new user
+      console.log("Registered :", user)
+    }
+
+   
+  }
+
   return <>
   <FormContainer>
     <Section>
       <Label>Create an account</Label>
-        <Row>
-          <StyledInput 
-            placeholder="First name"
-            value={user.firstName}
-            onChange={e => setUser({...user, firstName: e.target.value})}
-          />
-          <StyledInput 
-            placeholder="Last name"
-            value={user.lastName}
-            onChange={e => setUser({...user, lastName: e.target.value})}
-          />
-        </Row>
+      {error && <Error>{errorText}</Error>}
+      <Row>
         <StyledInput 
-            placeholder="Email address"
-            value={user.email}
-            onChange={e => setUser({...user, email: e.target.value})}
+          placeholder="First name"
+          value={user.firstName}
+          status={errorText.includes('first name') ? 'error' : ''}
+          onChange={e => setUser({...user, firstName: e.target.value})}
+        />
+        <StyledInput 
+          placeholder="Last name"
+          value={user.lastName}
+          status={errorText.includes('last name') ? 'error' : ''}
+          onChange={e => setUser({...user, lastName: e.target.value})}
           />
-          <StyledInput
-            type='password'
-            placeholder="Create password"
-            value={user.password}
-            onChange={e => setUser({...user, password: e.target.value})}
-          />
-          <Radio.Group onChange={e => setUser({...user, role: e.target.value})} value={user.role}>
-            <Radio value={"student"}>Student</Radio>
-            <Radio value={"staff"}>Staff</Radio>
-          </Radio.Group>
-          <ColoredButton
-            onClick={(e: any) => console.log("Register")}
-          >
-            Register
-          </ColoredButton>
+      </Row>
+      <StyledInput 
+        placeholder="Email address"
+        value={user.email}
+        status={errorText.includes('email address') ? 'error' : ''}
+        onChange={e => setUser({...user, email: e.target.value})}
+      />
+      <StyledInputPassword
+        type='password'
+        placeholder="Create password"
+        value={user.password}
+        status={errorText.includes('password') ? 'error' : ''}
+        onChange={e => setUser({...user, password: e.target.value})}
+      />
+        <StyledInputPassword
+        type='password'
+        placeholder="Confirm password"
+        value={confirmPassword}
+        status={errorText.includes('password') || confirmPassword === user.password ? '' : 'warning'}
+        onChange={e => setConfirmPassword(e.target.value)}
+      />
+      <Radio.Group onChange={e => setUser({...user, role: e.target.value})} value={user.role}>
+        <Radio value={"student"}>Student</Radio>
+        <Radio value={"staff"}>Staff</Radio>
+      </Radio.Group>
+        <ColoredButton
+          onClick={e => register(user)}
+        >
+          Register
+        </ColoredButton>
       </Section>
     </FormContainer>
   </>
@@ -100,6 +152,11 @@ const StyledInput = styled(Input)`
   width: 100%;
 `;
 
+const StyledInputPassword = styled(Input.Password)`
+  border-radius: 3px;
+  width: 100%;
+`;
+
 const ColoredButton = styled(Button)`
   border-radius: 3px;
   width: 150px;
@@ -111,4 +168,12 @@ const ColoredButton = styled(Button)`
     background-color: #2c9eb5c7;
     border-color: #2c9eb5c7 !important;
   }
+`;
+
+const Error = styled.div`
+  margin: -15px 0px;
+  color: red;
+  background-color: #fd83838d;
+  font-size: 12px;
+  padding: 2px 0;
 `;

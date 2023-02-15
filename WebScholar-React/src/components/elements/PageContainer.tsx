356b668b025371f6976b045d89261bd230d2d752
BrandonShapiro <brandonshapiro9@gmail.com>
2023-02-15 03:04:50 -0700

    updated style

--- WebScholar-React/src/components/elements/PageContainer.tsx
+++ WebScholar-React/src/components/elements/PageContainer.tsx
@@ -6,9 +6,7 @@ export const PageContainer = styled.div`
   align-items: center;
   margin: auto;
   align-self: center;
-  height: 100% - 50px;
   min-height: 550px;
-  width: 100% - 50px;
   min-width: 1000px;
   background-color: white;
   padding: 25px;

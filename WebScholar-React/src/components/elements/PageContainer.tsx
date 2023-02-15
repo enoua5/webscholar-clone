4a9ce9dfffa387942c0533215d453a11dc153b23
BrandonShapiro <brandonshapiro9@gmail.com>
2023-02-15 01:50:43 -0700

    updated page container style

---
+++ WebScholar-React/src/components/elements/PageContainer.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: auto;
+  align-self: center;
+  height: 100% - 50px;
+  min-height: 550px;
+  width: 100% - 50px;
+  min-width: 1000px;
+  background-color: white;
+  padding: 25px;
+`;

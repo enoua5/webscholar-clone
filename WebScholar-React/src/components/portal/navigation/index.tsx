92adb1279b41903372aa441fd98476511ca0a48b
BrandonShapiro <brandonshapiro9@gmail.com>
2023-02-15 01:55:52 -0700

    dynamic navigation buttons for students/staff

--- WebScholar-React/src/components/portal/navigation/index.tsx
+++ WebScholar-React/src/components/portal/navigation/index.tsx
@@ -1,17 +1,19 @@
-import { Avatar } from 'antd';
 import React from 'react';
-import { useNavigate } from 'react-router';
-import styled from 'styled-components'
-import { useAppDispatch, useAppSelector } from '../../../hooks';
-import { navPopoutOpen, setNavigationState } from '../../../state/reducers/navigationSlice';
+import { Avatar } from 'antd';
 import NavButton from './navButton';
 import NavPopout from './navPopout';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router';
 import WebScholarLogo from '../../../assets/logo.png';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
+import { setDefaultUser, userState } from '../../../state/reducers/userSlice';
+import { navPopoutOpen, setNavigationState } from '../../../state/reducers/navigationSlice';
 
 function Navigation() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const _navPopoutOpen = useAppSelector(navPopoutOpen);
+  const user = useAppSelector(userState);
 
   const handleClick = (menu: string, submenu: string, popoutOpen: boolean) => {
     dispatch(setNavigationState({
@@ -21,47 +23,100 @@ function Navigation() {
     }))
   };
 
+  let NavButtons: React.ReactElement[] = [
+    <NavButton
+      key="Home"
+      label="Home"
+      type="underline"
+      handleClick={() => {
+        handleClick("Home", "", false);
+        navigate("/");
+      }}
+    />,
+    <NavButton
+      key="About"
+      label="About"
+      type="underline"
+      handleClick={() => {
+        handleClick("About", "", false);
+        navigate("/about");
+      }}
+    />
+  ];
+
+  let studentNavigation: React.ReactElement[] = [
+    <NavButton
+      key="Scholarships" 
+      label="Scholarships"
+      type="underline"
+      handleClick={() => {
+        handleClick("Scholarships", "", false);
+        navigate("/scholarships");
+      }}
+    />
+  ];
+
+  let staffNavigation: React.ReactElement[] = [
+    <NavButton
+      key="Review" 
+      label="Review"
+      type="underline"
+      handleClick={() => {
+        handleClick("Review", "", false);
+        navigate("/review-applicants");
+      }}
+    />
+  ];
+
+  const LoginRegisterButtons: React.ReactElement[] = [
+    <NavButton
+      key="Log in" 
+      label="Log in"
+      type="underline" 
+      handleClick={() => {
+        handleClick("Log in", "", false);
+        navigate("/login");
+      }}
+    />,
+    <NavButton
+      key="Start for free"
+      label="Start for free"
+      type="box" 
+      handleClick={() => {
+        handleClick("Start for free", "", false);
+        navigate("/register");
+      }}
+    />
+  ];
+
+  const LogoutButton = 
+    <NavButton 
+      label="Logout"
+      type="box" 
+      handleClick={() => {
+        dispatch(setDefaultUser());
+        navigate("/");
+      }}
+    />
+
+  if(user.role === 'student'){
+    studentNavigation.forEach(button => NavButtons.push(button))
+  }
+
+  if(user.role === 'staff'){
+    staffNavigation.forEach(button => NavButtons.push(button))
+  }
+
   return (
   <>
     <NavigationBar>
       <Section>
         <Avatar src={WebScholarLogo} style={{height: "65px", width: "65px"}}/>
         <WebScholarButton onClick={() => navigate("/")}>WebScholar</WebScholarButton>
-        <NavButton 
-          label="Home"
-          type="box"
-          handleClick={() => {
-            handleClick("Home", "", false);
-            navigate("/");
-          }}
-        />
-        <NavButton 
-          label="About"
-          type="box"
-          handleClick={() => {
-            handleClick("About", "", false);
-            navigate("/about");
-          }}
-        />
+        {NavButtons}
       </Section>
       <Section>
-      
-        <NavButton 
-          label="Log in"
-          type="box" 
-          handleClick={() => {
-            handleClick("Log in", "", false);
-            navigate("/login");
-          }}
-        />
-        <NavButton 
-          label="Start for free"
-          type="round" 
-          handleClick={() => {
-            handleClick("Start for free", "", false);
-            navigate("/register");
-          }}
-        />
+       {user.active ? LogoutButton : LoginRegisterButtons}
       </Section>
     </NavigationBar>
 
@@ -82,7 +137,8 @@ const NavigationBar = styled.div`
   padding: 0 30px;
   min-width: 900px;
   box-shadow: 0px 6px 10px #b9b9b9;
-  z-index: 1;
+  background-color: white;
+  z-index: 10;
 `;
 
 const Section = styled.div`

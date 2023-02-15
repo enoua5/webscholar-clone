5a918b547a179866f9cf2a4e93f1010a5951c86f
BrandonShapiro <brandonshapiro9@gmail.com>
2023-02-15 01:55:33 -0700

    updated to ant design pro footer

--- WebScholar-React/src/components/portal/footer/index.tsx
+++ WebScholar-React/src/components/portal/footer/index.tsx
@@ -1,31 +1,10 @@
-import styled from 'styled-components';
+import { DefaultFooter } from "@ant-design/pro-layout";
 
-function Footer() {
-  const thisYear: number = new Date().getFullYear();
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <FooterBar>
-      <FooterText>
-        &copy; {thisYear  + " - WebScholar"}
-      </FooterText>
-    </FooterBar>
-  )
+    <DefaultFooter
+      copyright={`WebScholar ${currentYear}`}
+    />
+  );
 }
-
-export default Footer
-
-const FooterBar = styled.div`
-  width: 100%;
-  height: 35px;
-  background-color: #CDD7DF;
-  display: flex;
-  justify-self: flex-end;
-`;
-
-const FooterText = styled.div`
-  display: flex;
-  align-self: center;
-  margin: 0 30px 0 auto;
-  font-size: 13px;
-  font-family: sans-serif;
-  font-weight: 200;
-`;

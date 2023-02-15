3fb7a74711dbc5cdcf67e9a06f57f8c489b3707c
BrandonShapiro <brandonshapiro9@gmail.com>
2023-02-15 01:56:18 -0700

    dynamic routes for students/staff, can only visit appropriate pages

--- WebScholar-React/src/components/portal/main/index.tsx
+++ WebScholar-React/src/components/portal/main/index.tsx
@@ -1,27 +1,50 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
+import AboutPage from '../../pages/AboutPage';
 import AccountRecovery from '../../pages/AccountRecoveryPage';
-import HomePage from '../../pages/HomePage';
+import WelcomePage from '../../pages/WelcomePage';
 import LoginPage from '../../pages/LoginPage';
 import RegistrationPage from '../../pages/RegistrationPage';
 import Footer from '../footer';
+import HomePage from '../../pages/HomePage';
+import { useAppSelector } from '../../../hooks';
+import { userState } from '../../../state/reducers/userSlice';
+import DummyPage from '../../pages/DummyPage';
 
 function Main() {
+  const user = useAppSelector(userState);
 
   let menu: React.ReactElement[] = [
-    <Route key='/' path='/' element={<HomePage />} />,
+    <Route key='/' path='/' element={user.email !== "" ? <HomePage /> : <WelcomePage />} />,
     <Route key='/login' path="/login" element={<LoginPage />} />,
     <Route key='/register' path="/register" element={<RegistrationPage />} />,
     <Route key='/account-recovery' path='/account-recovery' element={<AccountRecovery />} />,
+    <Route key='/about' path="/about" element={<AboutPage />} />,
   ];
+
+  const studentMenu: React.ReactElement[] = [
+    <Route key='/scholarships' path="/scholarships" element={<DummyPage />} />,
+  ];
+
+  const administratorMenu: React.ReactElement[] = [
+    <Route key='/review-applicants' path="/review-applicants" element={<DummyPage />} />,
+  ]
+
+  if(user?.role === 'student'){
+    studentMenu.forEach(route => menu.push(route));
+  }
+
+  if(user?.role === 'staff'){
+    administratorMenu.forEach(route => menu.push(route));
+  }
 
   return (
     <MainContainer>
       <Routes>
        {menu}
       </Routes>
-      <Footer />
+      <Footer/>
     </MainContainer>
   );
 }
@@ -31,10 +54,11 @@ export default Main
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: calc(100vh - 65px);
-  width: 100%;
+  height: 94%;
+  width: 94%;
   min-width: 1000px;
   position: relative;
   overflow: auto;
   background-color: #f3f3f3;
+  padding: 3%;
 `;

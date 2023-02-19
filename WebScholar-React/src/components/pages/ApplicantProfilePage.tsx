06b46cf4b9c6c48ba161e67dd51a7b4841d8da39
BrandonShapiro <brandonshapiro9@gmail.com>
2023-02-18 21:57:50 -0700

    created applicant profile page (contains tabs to enter relevant information for each category)

--- WebScholar-React/src/components/pages/ApplicantProfilePage.tsx
+++ WebScholar-React/src/components/pages/ApplicantProfilePage.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { Space, Tabs } from 'antd'
+import styled from 'styled-components'
+import { Header } from '../elements/Header'
 
 export default function ApplicantProfilePage() {
-  return (
-    <div>Applicant Profile Page</div>
-  )
+  return <>
+  <Header>Application Profile</Header>
+    <Spacer />
+    <Tabs
+      tabPosition='left'
+      items={[
+        { 
+          label: "Personal Information",
+          key: "personal",
+          children: <></>
+        },
+        { 
+          label: "Academic History",
+          key: "academic",
+          children: <></>
+        },
+        { 
+          label: "Extracurricular Activities",
+          key: "extracurricular",
+          children: <></>
+        },
+        { 
+          label: "Awards and Accomplishments",
+          key: "awards",
+          children: <></>
+        },
+        { 
+          label: "About You",
+          key: "about",
+          children: <></>
+        }
+      ]}
+    />
+  </>
 }
+
+
+const Spacer = styled.div`
+  height: 50px;
+`;

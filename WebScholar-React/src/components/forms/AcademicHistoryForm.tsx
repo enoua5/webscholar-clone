e6ec4f04290adb35b6cb30520ce9dc25af8d574d
BrandonShapiro <brandonshapiro9@gmail.com>
2023-02-20 02:17:37 -0700

    outline what needs to be done

--- WebScholar-React/src/components/forms/AcademicHistoryForm.tsx
+++ WebScholar-React/src/components/forms/AcademicHistoryForm.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { FormContainer } from './PersonalInfoForm'
+import { FormContainer, FormHeader } from './PersonalInfoForm'
 
 export default function AcademicHistoryForm() {
   return (
     <FormContainer>
-      
+      <FormHeader>Education</FormHeader>
+
+      {/* High School Name */}
+      {/* High School Graduation Date */}
+      {/* High School GPA */}
+      {/* Upload High School Transcript */}
+
+      {/* What school will you be attending in the upcoming semester */}
+      {/* I will be attending as a... Freshman, Sophomore, Junior, Senior (Classification)*/}
+      {/* Current GPA if applicable (on a 4.0 scale)*/}
+      {/* Upload College Transcript */}
+      {/* What specialty or major do you plan to pursue?*/}
+      {/* Estimated date of graduation */}
+
     </FormContainer>
   )
 }

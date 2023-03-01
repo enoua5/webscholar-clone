b41395cffc807d0ddaa96cc5059fb0d05c51de05
Daniel Oldham <danieloldham@mail.weber.edu>
2023-02-28 17:05:25 -0700

    edit-profile-form now passes data correctly to backend, still need to fix account key in url though

--- frontend/src/app/edit-profile-form/edit-profile.service.ts
+++ frontend/src/app/edit-profile-form/edit-profile.service.ts
@@ -2,8 +2,6 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-const INSERT_URL = 'http://localhost:6001/accounts';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -12,8 +10,16 @@ export class EditProfileService{
 
   }
 
-  public insert(data): Observable<any> {
-    return this.http.post(INSERT_URL, data);
-  }
+  public updateAccount(data){
+    let header = new HttpHeaders({  'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    });
 
+    //checking if data is not null
+    console.log(data);
+
+    // TODO: figure out how to get the current account key, then pass it as part of the url
+    return this.http.post<any>('http://localhost:6001/account/update/1', data, { headers: header, observe: 'response', responseType: 'json'}).pipe();
+  }
 }

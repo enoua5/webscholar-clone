afe4598c4c032c3e90a03e9216ffa87aa837c827
Nathan C Knapczyk <nathanknapczyk@mail.weber.edu>
2023-02-28 19:10:24 -0700

    When going to committee page from scholarship-list it sends the scholarshipId. Update the committee page (may need rename to better reflect what the page is for exactly). Shows a dummy list of applicants for that application. Displays the name, appId, schoolId, and current score. When going to the scoring page, will send the application id.

--- frontend/src/app/scholarship-committee/committee.component.ts
+++ frontend/src/app/scholarship-committee/committee.component.ts
@@ -6,9 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./committee.component.less']
 })
 export class CommitteeComponent implements OnInit {
+  public applicants: any[];
+  public scholarship: any[];
 
+  constructor() {
+    this.applicants = [
+      {
+        name: "John Smith",
+        applicationId: 123456,
+        schoolId: "W0123456",
+        applicationScore: null,
+      },
+      {
+        name: "Jane Smith",
+        applicationId: 234567,
+        schoolId: "W1234567",
+        applicationScore: 3
+      },
+      {
+        name: "Jack Black",
+        applicationId: 345678,
+        schoolId: "W2345678",
+        applicationScore: 5
+      },
+    ];
 
-  constructor() { }
+    this.scholarship = [
+      {
+        scholarshipId: 1,
+        title: 'Scholarship 1',
+        organization: 'Organization 1',
+        description: 'Description 1',
+        requirements: 'Requirements 1',
+        amount: '100'
+      },
+    ];
+  }
 
   ngOnInit(): void {
   }

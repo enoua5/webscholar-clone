b7fe4b28008ec3b3d8ea902e6905d8625f0ce704
Nathan C Knapczyk <nathanknapczyk@mail.weber.edu>
2023-03-01 10:35:07 -0700

    small changes made: On the Committee page, OnInit the scholarship title will be set based on the passed scholarshipID (will need api call to achieve properly). Currently it sets the title to a constant 'Scholarship 1' onInit. Also added some comments in the OnInit to better explain the goal of this page and a possible method of how to achieve that goal once api calls are created to allow for it. Not sure if my method is the best method at this time, would require proper testing and implementation of api calls to find optimal method

--- frontend/src/app/scholarship-committee/committee.component.ts
+++ frontend/src/app/scholarship-committee/committee.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommitteeComponent implements OnInit {
   public applicants: any[];
-  public scholarship: any[];
+  public scholarshipTitle: any;
 
   constructor() {
     this.applicants = [
@@ -30,20 +30,21 @@ export class CommitteeComponent implements OnInit {
         applicationScore: 5
       },
     ];
-
-    this.scholarship = [
-      {
-        scholarshipId: 1,
-        title: 'Scholarship 1',
-        organization: 'Organization 1',
-        description: 'Description 1',
-        requirements: 'Requirements 1',
-        amount: '100'
-      },
-    ];
   }
 
   ngOnInit(): void {
-  }
+    this.scholarshipTitle = "Scholarship 1";
 
+    // first use the passed scholarshipId to get the scholarship information from the db.
+    // next use the scholarshipID as a means of pulling a list of all applications for the scholarship
+    // along with this take the user ID associated with each application to create a list of the applicants information
+    // since every application has an applicant this will allow us to have 2 lists of the same length
+    // this will then allow us to display certain information associated with the user as well as their application
+    // this may require modifying how it iterates through the lists to be a single for loop based on the count of applicants
+
+    // this wil also requre doing something similar for the getting the current score of the applicant.
+    // another option could be creating a function that is called each iteration that will set the values
+    //   for the user info and score, although I am not sure which would be more intensive at time of running the page
+    //   at this time, will require testing to see which method will provide the best results for user.
+  }
 }

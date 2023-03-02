57a5cfdc2e3563614a835a78d120c192cbeb1e3d
Jacob Allen <enoua5@gmail.com>
2023-03-01 20:52:17 -0700

    Give backend the issue object in the correct format

--- frontend/src/app/report-issue-form/report-issue-form.service.ts
+++ frontend/src/app/report-issue-form/report-issue-form.service.ts
@@ -15,7 +15,16 @@ export class ReportIssueFormService {
   }
 
   createIssue(issue: Iissue) {
-    let body = JSON.stringify(issue);
+    // make a new object to free us from the bonds of TypeScript
+    let body_obj = Object({...issue})
+    // Add in some data that the user doesn't get to choose
+    body_obj.reporterId = parseInt(sessionStorage.getItem('accountKey'));
+    body_obj.status = "Unreviewed"
+    // this field has a different name in the backend
+    body_obj.stepsToReCreate = body_obj.recreate;
+    delete body_obj.recreate;
+    // Body needs to be a string
+    let body = JSON.stringify(body_obj);
     console.log(body);
     return this.http.post('http://localhost:6005/issue/createIssue', body, httpOptions);
   }

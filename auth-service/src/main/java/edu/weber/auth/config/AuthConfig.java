1bbc3c51aca5b17cb243a0eed82d8c8eb9a3d72b
20dshakespear <20dshakespear@gmail.com>
2023-03-14 10:40:46 -0600

    Added documentation

--- auth-service/src/main/java/edu/weber/auth/config/AuthConfig.java
+++ auth-service/src/main/java/edu/weber/auth/config/AuthConfig.java
@@ -16,33 +16,49 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.sql.DataSource;
 
-/*
- * This class configures the authorization server
+/**
+ * This class sets up the Authorization Server using Spring Security
  */
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthConfig extends AuthorizationServerConfigurerAdapter {
 
+    // JDBC data to configure the client access
     @Autowired
     private DataSource dataSource;
 
+    // Store and manage tokens used by auth-service
     @Autowired
     private TokenStore tokenStore;
 
+    // Converts tokens to JWT
     @Autowired
     private JwtAccessTokenConverter accessTokenConverter;
 
+    /**
+     * Configures the endpoints of auth-services
+     * Sets the Token store as tokenStore
+     *
+     * @param endpoints AuthorizationServerEndpointsConfigurer
+     * @throws Exception if an error occurs while configuring the endpoints
+     */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore)
                 .accessTokenConverter(accessTokenConverter);
     }
 
+    /**
+     * Configures the clients that are authorized to access the server
+     * Sets up JDBC-based client details service that reads the client details from the source.
+     *
+     * @param clients ClientDetailsServiceConfigurer
+     * @throws Exception if an error occurs while configuring
+     */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .jdbc(dataSource);
     }
-
 }
--- auth-service/src/main/java/edu/weber/auth/config/AuthResourceConfig.java
+++ auth-service/src/main/java/edu/weber/auth/config/AuthResourceConfig.java
@@ -11,20 +11,32 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+/**
+ * Defines the Beans required for JWT token authentication
+ */
 @Configuration
 @EnableResourceServer
 public class AuthResourceConfig extends ResourceServerConfigurerAdapter {
 
+    /**
+     * Makes ResourceServerSecurityConfigurer use the bean in this class
+     */
     @Override
     public void configure(ResourceServerSecurityConfigurer config) {
         config.tokenServices(tokenServices());
     }
 
+    /**
+     * Creates a bean for the TokenStore to store and get JWT tokens.
+     */
     @Bean
     public TokenStore tokenStore() {
         return new JwtTokenStore(accessTokenConverter());
     }
 
+    /**
+     * Creates a bean for JwtAccessTokenConverter to encode and decode JWT tokens.
+     */
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
@@ -32,6 +44,10 @@ public class AuthResourceConfig extends ResourceServerConfigurerAdapter {
         return converter;
     }
 
+    /**
+     * Creates a bean for the DefaultTokenServices
+     * It's the "Default" bean
+     */
     @Bean
     @Primary
     public DefaultTokenServices tokenServices() {

0ae4c6599d6b5337c45b7aee418aa800a8906161
Abigail Goodwin <abby.goodwin@outlook.com>
2023-03-20 17:47:23 -0600

    Updated all unit tests save for edit-profile.

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+
+// Component-Specific Imports
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +12,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent
       ],
     }).compileComponents();
   });
--- frontend/src/app/change-password-form/change-password-form.component.spec.ts
+++ frontend/src/app/change-password-form/change-password-form.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+// Component-Specific Imports
 import { ChangePasswordFormComponent } from './change-password-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ChangePasswordFormComponent', () => {
   let component: ChangePasswordFormComponent;
@@ -8,6 +10,7 @@ describe('ChangePasswordFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ ReactiveFormsModule ],
       declarations: [ ChangePasswordFormComponent ]
     })
     .compileComponents();

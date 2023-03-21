0f2c7f3126202325f1e8204ab3a0de1e654283ad
Artem Koval <arty.nar00@gmail.com>
2023-03-21 17:05:08 -0600

    Added documentation for two of the services, fixed the compodoc syntax

--- frontend/src/app/terms-conditions/terms-conditions.component.ts
+++ frontend/src/app/terms-conditions/terms-conditions.component.ts
@@ -1,22 +1,39 @@
 import {Component, OnInit} from '@angular/core';
 import {TermsConditionsService} from './terms-conditions.service';
 
+/**
+ * Terms Conditions Component
+ */
 @Component({
     selector: 'terms-conditions',
     templateUrl: './terms-conditions.component.html',
     styleUrls: ['./terms-conditions.component.less']
 })
 
+/**
+ * Interacts with the invitation form on the page
+ */
 export class TermsConditionsComponent implements OnInit {
-    // Declares a service
+    /**
+     * Empty constructor (Nothing on a page to interact with)
+     * @param service TermsConditionsService dependency
+     */
     constructor(private service: TermsConditionsService) {}
 
+    /**
+    * Runs when page is loaded, makes an API call
+    */
     ngOnInit(): void {
         // TODO: This should send a request to the backend service that would provide
         // terms and conditions data, instead of having it hard-coded as it is nows
         this.service.retrieve().subscribe((data) => this.processResponse(data));  
     }
-
+    
+    /**
+     * Runs when the response is received from the backend
+     * TODO: Has to populate the info on the page with data returned from backend
+     * @param data data received from backend
+     */
     private processResponse(data)
     {
         // TODO: backend should send us back the requested data

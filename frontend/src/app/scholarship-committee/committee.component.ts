f5a72dea3b9707f9690cd896b9a0825b08f90ba4
Nathan C Knapczyk <nathanknapczyk@mail.weber.edu>
2023-03-27 12:18:18 -0600

    filtering on whether application is scored or not. 2 options: All applications, and applications with no score.

--- frontend/src/app/scholarship-committee/committee.component.ts
+++ frontend/src/app/scholarship-committee/committee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { forEach } from 'cypress/types/lodash';
 
 @Component({
   selector: 'app-committee',
@@ -7,12 +8,13 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./committee.component.less']
 })
 export class CommitteeComponent implements OnInit {
-  public applicants: any[];
+  public allApplicants: any[];
+  public displayedApplicants: any[];
   public scholarshipTitle: any;
   public scholarId: any;
 
   constructor(private _Activedroute:ActivatedRoute) {    
-    this.applicants = [
+    this.allApplicants = [
       {
         name: "John Smith",
         applicationId: 123456,
@@ -38,6 +40,8 @@ export class CommitteeComponent implements OnInit {
     this.scholarId = this._Activedroute.snapshot.paramMap.get("id");
     this.scholarId = this.scholarId as number;
     this.scholarshipTitle = "Scholarship " + this.scholarId;
+    this.displayedApplicants = Object.assign([], this.allApplicants)
+
 
     // first use the passed scholarshipId to get the scholarship information from the db.
     // next use the scholarshipID as a means of pulling a list of all applications for the scholarship
@@ -47,8 +51,20 @@ export class CommitteeComponent implements OnInit {
     // this may require modifying how it iterates through the lists to be a single for loop based on the count of applicants
 
     // this wil also requre doing something similar for the getting the current score of the applicant.
-    // another option could be creating a function that is called each iteration that will set the values
+    // Another option could be creating a function that is called each iteration that will set the values
     //   for the user info and score, although I am not sure which would be more intensive at time of running the page
     //   at this time, will require testing to see which method will provide the best results for user.
   }
+
+  filterApps(filter): void {
+    if (filter == 0) {
+      this.displayedApplicants.splice(0);
+      this.displayedApplicants = Object.assign([], this.allApplicants)
+    }
+    else if (filter == 1) {
+      this.displayedApplicants = this.displayedApplicants.filter((app) => app.applicationScore === null)
+    }
+  }
+
+
 }

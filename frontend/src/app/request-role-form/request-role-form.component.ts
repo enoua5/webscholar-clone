396348e9fcbc71cedbd159ac5936b548cc825ee0
Abigail Goodwin <abby.goodwin@outlook.com>
2023-03-29 17:20:56 -0600

    Updating angular.json to allow debugging in browser; added success/error messages for request role.

--- frontend/src/app/request-role-form/request-role-form.component.ts
+++ frontend/src/app/request-role-form/request-role-form.component.ts
@@ -2,22 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { RequestRoleService } from './request-role.service';
 
-// Component Metadata
+/**
+ * Metadata for the RequestRoleFormComponent.
+ */
 @Component({
   selector: 'request-role-form',
   templateUrl: './request-role-form.component.html'
 })
 
+/**
+ * Defines the RequestRoleFormComponent class.
+ * 
+ * @details This component allows users to request a new role.
+ */
 export class RequestRoleFormComponent implements OnInit 
 {
-  // Component Attributes
+  /**
+   * The title to display on the page tied to this component.
+   */
   pageTitle: string = "Request Role";
+
+  /**
+   * A list of the possible roles that users can apply for.
+   */
   roles: string[] = ['Committee Member', 'Committee Chair'];
+
+  /**
+   * The dropdown menu that is displayed to the user.
+   */
   roleForm = new FormGroup({
     roleControl: new FormControl('Committee Member', [Validators.required])
   });
 
-  // Default constructor
+  /**
+   * This variable stores any errors generated during processing.
+   */
+  errorMessage: string = null;
+
+  /**
+   * This variable indicates when a request was successfully received by the backend.
+   */
+  success: boolean = false;
+
+  /**
+   * Constructor for the RequestRoleFormComponent.
+   * 
+   * @param service The RequestRoleService object to use for this component.
+   */
   constructor(private service: RequestRoleService) {}
 
   // Component Methods
@@ -26,14 +57,33 @@ export class RequestRoleFormComponent implements OnInit
     // TODO: get currently logged-in user's email to send to backend with request.
   }
 
+  /**
+   * Called when the user submits the form. Sends their role request to the backend.
+   */
   onSubmit(): void
   {
-    this.service.sendRequest(this.roleForm.value).subscribe((data) => this.processResponse(data));
+    // Clear error message & success in case different results are generated each time.
+    this.errorMessage = null;
+    this.success = false;
+
+    const selectedRole: string = this.roleForm.controls['roleControl'].value;
+    this.service.sendRequest(selectedRole).subscribe(
+      {
+        next: () => this.success = true,
+        error: (error) => this.processErrors(error)
+      });
   }
 
-  private processResponse(data)
+  /**
+   * Parses the response from the backend to determine what errors need to be displayed.
+   * 
+   * @param response The HTTPErrorResponse given to us from the request_role endpoint.
+   */
+  private processErrors(response)
   {
-    // TODO: backend should report if the user already has an existing request.
-    console.log(data);
+    if (response.status != 200)
+    {
+      this.errorMessage = response.error.message;
+    }
   }
 }

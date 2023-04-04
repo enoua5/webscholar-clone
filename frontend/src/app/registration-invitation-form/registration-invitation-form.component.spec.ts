805a5ead1ad2e7af4f919c939a70044fe129cf53
Artem Koval <arty.nar00@gmail.com>
2023-04-04 13:55:36 -0600

    added some unit-tests

--- frontend/src/app/registration-invitation-form/registration-invitation-form.component.spec.ts
+++ frontend/src/app/registration-invitation-form/registration-invitation-form.component.spec.ts
@@ -7,10 +7,17 @@ import { RegistrationInvitationFormComponent } from './registration-invitation-f
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { RegistrationInvitationService } from './registration-invitation.service';
+import { of, Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('RegistrationInvitationFormComponent', () => {
   let component: RegistrationInvitationFormComponent;
   let fixture: ComponentFixture<RegistrationInvitationFormComponent>;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let testService: RegistrationInvitationService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,9 +31,81 @@ describe('RegistrationInvitationFormComponent', () => {
     fixture = TestBed.createComponent(RegistrationInvitationFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    testService = new RegistrationInvitationService(httpClientSpy as any);
+    router = TestBed.inject(Router); // inject the router
+
+    sessionStorage.setItem('accountKey', '1')
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display' , () => {
+    const element = fixture.debugElement.nativeElement;
+    expect(element.textContent).toContain("Send");
+  })
+
+  it('correct number of input fields' , () => {
+    const formElement = fixture.debugElement.nativeElement.querySelector('form')
+    const inputElements = formElement.querySelectorAll('input')
+    expect(inputElements.length).toEqual(1)
+  })
+
+  it('value by default is empty' , () => {
+    const pageForm = component.form;
+    const pageFormValue = {emails:''};
+    expect(pageForm.value).toEqual(pageFormValue);
+  })
+
+  it('component does email validation bad' ,  () => {
+    const pageForm = component.form;
+    pageForm.setValue({emails: "bad_EmAiL"});
+
+    component.onSubmit()
+
+    expect(component.errors.get('emails')).toEqual('Invalid email');
+    expect(component.emails).toEqual(["bad_EmAiL"]);
+  })
+
+  it('component does email validation good' ,  () => {
+    const pageForm = component.form;
+    pageForm.setValue({emails: "test@test.test"});
+
+    component.onSubmit()
+
+    expect(component.errors.get('emails')).toBeFalsy();
+    expect(component.emails).toEqual(["test@test.test"]);
+  })
+
+  it('component does email validation multiple good' ,  () => {
+    const pageForm = component.form;
+    pageForm.setValue({emails: "test@test.test,anothertest@test.test"});
+
+    component.onSubmit()
+
+    expect(component.errors.get('emails')).toBeFalsy();
+    expect(component.emails).toEqual(["test@test.test", "anothertest@test.test"]);
+  })
+
+  it('component does email validation multiple bad' ,  () => {
+    const pageForm = component.form;
+    pageForm.setValue({emails: "test@test.test anothertest@test.test"});
+
+    component.onSubmit()
+
+    expect(component.errors.get('emails')).toEqual('Invalid email');  
+  })
+
+  it('test service functions' ,  () => {
+    const mockResponse = { status: 200 };
+    const emailArray = ["test@test.test", "anothertest@test.test"];
+
+    httpClientSpy.get.and.returnValue(of(mockResponse));
+    testService.sendEmail(emailArray).subscribe(response => {
+      expect(response.status).toBe(200);
+    });      
+  })
 });

092e9c1573d78fc4cae91bf27f2bd5d4e258b532
IsaacP-WSU <isaacpatterson@mail.weber.edu>
2023-04-05 16:47:18 -0600

    updated ScholarshipApplication model and added ScholarshipApplicationEvaluation model.

--- scholarship-application-service/src/main/java/edu/weber/model/ScholarshipApplication.java
+++ scholarship-application-service/src/main/java/edu/weber/model/ScholarshipApplication.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Column;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 public class ScholarshipApplication {
@@ -16,10 +18,6 @@ public class ScholarshipApplication {
     @GeneratedValue(
             strategy = GenerationType.IDENTITY
     )
-
-    /**
-     * Primary Key
-     */
     private int scholarshipApplicationId;
 
     /**
@@ -29,6 +27,14 @@ public class ScholarshipApplication {
     @Column(nullable = false)
     private int accountKey;
 
+    public int getAccountKey() {
+        return accountKey;
+    }
+
+    public void setAccountKey(int accountKey) {
+        this.accountKey = accountKey;
+    }
+
     /**
      * Foreign Key
      * Links to the scholarship found in the scholarship-service (ScholarshipApp)
@@ -36,10 +42,91 @@ public class ScholarshipApplication {
     @Column(nullable = false)
     private int scholarshipId;
 
+    public int getScholarshipId() {
+        return scholarshipId;
+    }
+
+    public void setScholarshipId(int scholarshipId) {
+        this.scholarshipId = scholarshipId;
+    }
+
+    /**
+     * Status of the application
+     * Approved, Accepted, Pending, Rejected, InProgress, etc...
+     */
+    @Column
+    public String scholarshipApplicationStatus;
+
+    public String getScholarshipApplicationStatus() {
+        return scholarshipApplicationStatus;
+    }
+
+    public void setScholarshipApplicationStatus(String scholarshipApplicationStatus) {
+        this.scholarshipApplicationStatus = scholarshipApplicationStatus;
+    }
+
+    /**
+     * Date and time that the scholarshipApplication was created (submitted)
+     */
+    @Column
+    @CreationTimestamp
+    public LocalDateTime dateSubmitted;
+
+    public LocalDateTime getDateSubmitted() {
+        return dateSubmitted;
+    }
+
+    public void setDateSubmitted(LocalDateTime dateSubmitted) {
+        this.dateSubmitted = dateSubmitted;
+    }
+
+    // Date reviewed was moved to ScholarshipApplicationEvaluation as dateEvaluated.
+
+    /**
+     * This is the date that the final decision is made for the ScholarshipApplication
+     */
+    @Column
+    public LocalDateTime dateDecided;
+
+    public LocalDateTime getDateDecided() {
+        return dateDecided;
+    }
+
+    public void setDateDecided(LocalDateTime dateDecided) {
+        this.dateDecided = dateDecided;
+    }
+
+    /**
+     * Default constructor
+     */
     public ScholarshipApplication(){}
 
+    /**
+     * Custom constructor. Creates a ScholarshipApplicationEvaluation object.
+     * Parameters are values that must not be blank when this object is created.
+     * @param accountKey is the student account associated with the ScholarshipApplication
+     * @param scholarshipId is the scholarship for which the ScholarshipApplication is being submitted
+     * dateSubmitted should be generated on creation
+     */
     public ScholarshipApplication(int accountKey, int scholarshipId){
         this.accountKey = accountKey;
         this.scholarshipId = scholarshipId;
+    }
+
+    /**
+     * This method allows us to see the ScholarshipApplication information in the console.
+     * This is helpful for debugging problems or for logging.
+     *
+     * @return Returns the 'ScholarshipApplication' data formatted as a string.
+     */
+    @override
+    public String toString(){
+        return "ScholarshipApplication{ " +
+                "scholarshipApplicationID=" + scholarshipApplicationId + '\'' +
+                ", accountKey=" + accountKey + '\'' +
+                ", scholarshipId=" + scholarshipId + '\'' +
+                ", dateSubmitted=" + dateSubmitted + '\'' +
+                ", dateDecided=" + dateDecided + '\'' +
+                '}';
     }
 }

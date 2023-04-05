7e4b053132ed41348925aef5c652da70a5279d06
Jeremy Krans <49561917+Jkrans@users.noreply.github.com>
2023-04-04 20:57:42 -0600

    Added services to scholarship-committee. Updated committee.component.ts to pull data from the scholarshipcommitteeservice. Updated committee.component.ts documentation to JSDocs

---
+++ frontend/src/app/scholarship-committee/scholarship-committee.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+/**
+ * ScholarshipCommitteeService is a service that handles the retrieval of
+ * scholarship applicant data for the scholarship committee component.
+ * It provides methods for fetching applicant data either from hard-coded
+ * data to test it or an API.
+ */
+export class ScholarshipCommitteeService {
+    private apiUrl = '';
+
+    /**
+     * applicants is an array of hard-coded scholarship applicant data for testing.
+     * This should be removed or commented out once the API is ready.
+     */
+    private applicants: any[] = [
+        {
+            name: "John Smith",
+            applicationId: 123456,
+            schoolId: "W0123456",
+            applicationScore: null,
+        },
+        {
+            name: "Jane Smith",
+            applicationId: 234567,
+            schoolId: "W1234567",
+            applicationScore: 3
+        },
+        {
+            name: "Jack Black",
+            applicationId: 345678,
+            schoolId: "W2345678",
+            applicationScore: 5
+        },
+        {
+            name: "Jim Halpert",
+            applicationId: 456789,
+            schoolId: "W2161657",
+            applicationScore: 4
+        },
+    ];
+
+    /**
+     * Constructs the ScholarshipCommitteeService instance and injects the HttpClient.
+     * @param http - The HttpClient instance for making API calls.
+     */
+    constructor(private http: HttpClient) { }
+
+    /**
+     * Fetches the scholarship applicant data either from hard-coded data or an API.
+     * @returns {Observable<any[]>} An Observable containing an array of scholarship applicant data.
+     */
+    getApplicants(): Observable<any[]> {
+        // Uncomment the line below to use the API when it's ready
+        // return this.http.get<any[]>(this.apiUrl);
+
+        // Comment or delete the line below when you start using the API
+        return of(this.applicants);
+    }
+
+    /**
+     * Fetches the scholarship information using the scholarship ID.
+     * @param scholarshipId - The ID of the scholarship.
+     * @returns {Observable<any>} An Observable containing the scholarship information.
+     */
+    getScholarshipInfo(scholarshipId: number): Observable<any> {
+        // Uncomment the line below to use the API when it's ready
+        // return this.http.get<any>(`${this.apiUrl}/scholarships/${scholarshipId}`);
+
+        // Comment or delete the line below when you start using the API
+        // For now, just return an object with a title as a sample
+        return of({ title: 'Scholarship ' + scholarshipId });
+    }
+}

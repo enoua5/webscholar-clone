5b378a661ac3f175b8dff84d9957f5540b36b556
20dshakespear <20dshakespear@gmail.com>
2023-04-05 12:34:22 -0600

    Added additional dependencies and got the generateToken method working

--- auth-service/src/main/java/edu/weber/auth/controller/AuthController.java
+++ auth-service/src/main/java/edu/weber/auth/controller/AuthController.java
@@ -1,31 +1,21 @@
 package edu.weber.auth.controller;
 
-import edu.weber.auth.config.AuthResourceConfig;
 import edu.weber.auth.repository.UserRepository;
-import edu.weber.auth.service.UserService;
 import edu.weber.auth.model.User;
 import edu.weber.auth.model.AuthResponse;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 
 /**
  * This class manages Authentication requests from the Auth Service
@@ -53,8 +43,6 @@ public class AuthController {
     /**
      * Logs in via the auth service to retrieve a JWT token
      **/
-
-    /** TODO: Uncomment imports for JwtBuilder
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User user) {
 
@@ -75,36 +63,22 @@ public class AuthController {
     }
 
     // The secret key used to sign the JWT token
-    private final String secret = "my-secret-key"; // TODO: Set up secret key
+    private final String secret = "mweifafd65ewf68awe47f98w4f6a5hj6ty65j4u6gy3il7a92gr1aer98arge49afg12a3weg1aw9ge8ae4g65asdf4a3s2df1wqa69ef8a9sd7as6df521a3w2ef4fwe98f4as56df1aw321fw6115k5jh65gjk54r56Kadf5s2d1fa56we8e8ftt87df4aw2e2w56e6ad5f841n15i1ui5tyut5uty4r1a2a3fd2"; // TODO: Needs to be changed. Not Secure here
 
     // Define the expiration time of the JWT token
     private final long expirationTime = 3600000; // 1 hour
 
     // Generate a JWT token based on the user credentials
-    private OAuth2AccessToken generateToken(User user) {
-        // Define the claims for the JWT token
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("sub", user.getUserId());
-        claims.put("username", user.getUserName());
-        claims.put("roles", user.getUserRole());
-
-        // Define the expiration date for the JWT token
-        Date now = new Date();
-        Date expirationDate = new Date(now.getTime() + expirationTime);
-
-        // Generate the JWT token with the claims and expiration date
-        JwtBuilder jwtBuilder = JwtHelper.jwtBuilder()
-                .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(expirationDate)
-                .signWith(secret);
-
-        Jwt jwt = jwtBuilder.compact();
-        String encodedJwt = jwt.getEncoded();
+    public OAuth2AccessToken generateToken(User user) {
+        Date expiration = new Date(System.currentTimeMillis() + expirationTime);
+        String encodedJwt = Jwts.builder()
+                .setSubject(String.valueOf(user.getUserId()))
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
 
         // Create an instance of DefaultOAuth2AccessToken and set the JWT token as its value
         DefaultOAuth2AccessToken accessToken = new DefaultOAuth2AccessToken(encodedJwt);
-
         return accessToken;
-    }**/
+    }
 }

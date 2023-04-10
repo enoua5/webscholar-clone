5924912ac818d0d4dcc310ad6225c5c44328c67c
Daniel Oldham <danieloldham@mail.weber.edu>
2023-04-10 12:32:32 -0600

    finished login logout e2e test, added baseURL to cypress.config.ts

--- frontend/cypress/e2e/loginLogout.spec.cy.ts
+++ frontend/cypress/e2e/loginLogout.spec.cy.ts
@@ -1,6 +1,5 @@
 describe('login logout correctly', () => {
-  beforeEach(() => {
-    // create mock account
+  it('should submit form correctly and be logged in', () => {
     cy.request('POST', 'http://localhost:6001/account/create', {
       email: "fake.guy@fakeemailserviceprovider.internet",
       password: "ASFDasdfasdf1!",
@@ -10,17 +9,45 @@ describe('login logout correctly', () => {
       firstName: "Fake",
       lastName: "Guy",
       school: "Weber State University"
-      });
-  })
-  it('should submit form correctly and be logged in', () => {
-    cy.visit('http://localhost:4200/');
+    });
+
+    // go to page
+    cy.visit('');
+
     // page should display
     expect(cy.get('h1').should('have.text', 'Log In'));
 
     // fill out values
     cy.get('#username').type('fake.guy@fakeemailserviceprovider.internet');
-    cy.get('#password').type('ASDFasdfasdf1!');
+    cy.get('#password').type("ASFDasdfasdf1!");
 
+    // wait for a sec, then submit
+    cy.wait(2000);
     cy.get('form').submit();
+
+    // should be on login page
+    expect(cy.get('.Edit_header').should('have.text', 'Your Dashboard'));
+
+    // name should be visible
+    expect(cy.get('a').contains('Welcome, ').should('have.text', 'Welcome, Fake Guy'));
+
+    // session storage should be set, can check with cy.window()
+    cy.window().then(win=> {
+      const someItem = win.sessionStorage.getItem('firstName');
+      cy.wrap(someItem).should('exist');
+    });
+
+    // wait for a sec, then logout
+    cy.wait(2000);
+    cy.get('a').contains('Logout').click();
+
+    // should be back to log in page
+    expect(cy.get('h1').should('have.text', 'Log In'));
+
+    // session storage should have been cleared out
+    cy.window().then(win=> {
+      const someItem = win.sessionStorage.getItem('firstName');
+      cy.wrap(someItem).should('not.exist');
+    });
   })
 })

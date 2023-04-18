fdc7db1cf216899ef9dd82dcc2a4988b8e94502f
Chappytits <danielchaplygin@gmail.com>
2023-04-17 18:28:06 -0600

    Unit test added to Score Applicants page
    This unit test tests if the correct number of search results are returned when using a search string. A collection of dummy applicants named John Doe, Jane Doe, and Bob Deer is created. The search string is then set to "doe". 2 search results are expected. If the length of displayedApplicants is 2, the test passes. The search string is then set to "bob". 1 search result is expected. If the length of displayedApplicants is 1, the test passes again. The unit test is then concluded.

--- frontend/src/app/scholarship-committee/committee.component.spec.ts
+++ frontend/src/app/scholarship-committee/committee.component.spec.ts
@@ -32,9 +32,36 @@ describe('ScholarshipCommitteeComponent', () => {
         fixture = TestBed.createComponent(CommitteeComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        component = new CommitteeComponent(null, null);
     });
 
-    it('should create', () => {
+  /**
+   * This unit test tests if the correct number of search results are returned when using a search string.
+   * A collection of dummy applicants named John Doe, Jane Doe, and Bob Deer is created. The search string is then set
+   * to "doe". 2 search results are expected. If the length of displayedApplicants is 2, the test passes. The search
+   * string is then set to "bob". 1 search result is expected. If the length of displayedApplicants is 1, the test
+   * passes again. The unit test is then concluded.
+   */
+  it('should return the correct number of searched applicants', () => {
         expect(component).toBeTruthy();
+        const applicants = [
+          { name: 'John Doe', schoolId: 'w1234567', appId: '123456' },
+          { name: 'Jane Doe', schoolId: 'w7654321', appId: '654321' },
+          { name: 'Bob Deer', schoolId: 'w2345678', appId: '345678' },
+      ];
+      component.allApplicants = applicants;
+
+      component.searchString = 'doe';
+      component.filterApps(0);
+      expect(component.displayedApplicants.length).toBe(2);
+
+      component.searchString = 'bob';
+      component.filterApps(0);
+      expect(component.displayedApplicants.length).toBe(1);
     });
+
+
+
+
+
 });
